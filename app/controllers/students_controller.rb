b9b5d357d3ee2f458e9f44403592e7220e21aca8
Ayush1998 <ayushmudgal11@gmail.com>
2020-08-28 10:26:44 +0530

    Added in student view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(params.require(:student).permit(:name, :age))
+    if @student.valid?
+      redirect_to students_path
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
   end
 
   def show
