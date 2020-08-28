2f85c14c7ae2725d4ac73a777298a92091861029
Ayush1998 <ayushmudgal11@gmail.com>
2020-08-28 11:23:13 +0530

    Added new course functionality

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,19 @@
 class CoursesController < ApplicationController
   def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(params.require(:course).permit(:name, :age))
+    if @course.valid?
+      redirect_to courses_path
+    else
+      flash[:errors] = @course.errors.full_messages
+      redirect_to new_course_path
+    end
   end
 end
