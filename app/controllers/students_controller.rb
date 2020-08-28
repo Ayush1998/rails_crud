8bfff9eb6c659c461475ee5912d924b83b9aa74c
Ayush1998 <ayushmudgal11@gmail.com>
2020-08-28 10:45:18 +0530

    Added in student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,14 +17,22 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:name, :age))
+    redirect_to students_path(@student)
   end
 
   def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
   end
 end
